7b7e5058a0e1cc9b2d7d28ea346119665e46193e
TengTeng123 <58301595+TengTeng123@users.noreply.github.com>
2019-12-01 00:08:28 +0800

    Add files via upload

---
+++ 16计2161501106818滕京耀二分查找.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+using namespace std;
+ 
+int a[100];
+int x;
+ 
+int BinarySearch(int a[],int x,int n)
+{//找到x时返回其在数组的位置，否则返回-1 
+	int left=0,right=n-1;
+	while(left<=right)
+	{
+		int middle=(left+right)/2;
+		if(x==a[middle])
+			return middle;
+		if(x<a[middle])
+			right=middle-1;	
+		else
+			left=middle+1;
+	}
+	return -1;//未找到x 
+}
+int main()
+{
+	int n;
+	cin>>n;//n个数 
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];	
+	}
+	cin>>x;//要找的数 
+	cout<<BinarySearch(a,x,n)<<endl;
+	return 0;	
+}
+
+
