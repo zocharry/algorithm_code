97354ac739758ce1e60b0700e21519061185718b
123faith123 <956702718@qq.com>
2019-12-01 09:45:23 +0800

    h

--- 17计科1班170201105169郑雄威.cpp
+++ 17计科1班170201105169郑雄威.cpp
@@ -1,31 +1,28 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-
-
 int BinarySearch(int *a, int x, int n){
-    int left = 0;
-    int right = n-1;
-    while(left <= right){
-        int middle = (left + right) / 2;
-        if(x == a[middle])
-            return middle;
-        if(x > a[middle])
-            left = middle + 1;
-        else
-            right = middle - 1;
-    }
-    return -1;
+	int left = 0;
+	int right = n-1;
+	while(left <= right){
+		int middle = (left + right) / 2;
+		if(x == a[middle])
+			return middle;
+		if(x > a[middle])
+			left = middle + 1;
+		else
+			right = middle -1;
+	}
+	return -1;
 }
-
 int main()
 {
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    int want_find;
-    cout << "输入你要找的数" << endl;
-    cin >> want_find;
-    int x = BinarySearch(a,want_find,10);
-    cout << x << endl;
-    return 0;
+	int a[10] = {1,2,3,4,5,6,7,8,9,10};
+	int want_find;
+	cout << "输入你要找的数" << endl;
+	cin >> want_find;
+	int x = BinarySearch(a,want_find,10);
+	cout << x << endl;
+	return 0; 
 }
