1f03ad9bdb8feae1ca7834208237a5bf968526bd
zbin <47966431+zbinyzb@users.noreply.github.com>
2019-12-01 10:13:14 +0800

    Create 17-计算机科学与技术-2班-170201103001游智斌-.c

---
+++ 17-计算机科学与技术-2班-170201103001游智斌-.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int binarySearch(int a[], const int& x, int n)
+{
+    int left=0, right=n-1;
+    while (left <= right)
+    {
+        int middle = (left+right)/2;
+        if (x==a[middle])
+        {
+            return middle;
+        }
+        if (x > a[middle])
+        {
+            left = middle+1;
+        }
+        else
+        {
+            right = middle-1;
+        }
+    }
+    return -1;
+}
+ 
+void main()
+{
+    int a[] = {1,3,5,7,11,14};
+    printf("%d\n",binarySearch(a,7,6));
+}
