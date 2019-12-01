203c20b64bcd1b7eaef459febafc6b9dc6520775
123faith123 <956702718@qq.com>
2019-12-01 09:55:40 +0800

    h

--- 17计科1班170201105169郑雄威.cpp
+++ 17计科1班170201105169郑雄威.cpp
@@ -20,7 +20,7 @@ int main()
 {
 	int a[10] = {1,2,3,4,5,6,7,8,9,10};
 	int want_find;
-	cout << ¡°please input want find¡± << endl;
+	cout<<¡°please input want find¡±<<endl;
 	cin >> want_find;
 	int x = BinarySearch(a,want_find,10);
 	cout << x << endl;
