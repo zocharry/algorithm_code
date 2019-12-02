143ab1e73f0489f3c443d9e474fcea2293d9afe2
mico_rouis <58336283+micorouis@users.noreply.github.com>
2019-12-02 11:34:35 +0800

    Create 18软工2班180021102002杨智睿.cpp

---
+++ 18软工2班180021102002杨智睿.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int Bin_Search(int *num,int cnt,int target)
+{
+ int first = 0,last = cnt-1,mid;
+ int counter = 0;
+ while(first <= last)
+ {
+  counter ++;
+  mid = (first + last) / 2;//确定中间元素 
+  if(num[mid] > target)
+  {
+   last = mid-1; //mid已经交换过了,last往前移一位
+  }
+  else if(num[mid] < target)
+  {
+   first = mid+1;//mid已经交换过了,first往后移一位
+  } 
+  else //判断是否相等
+  {
+   return mid;
+  }
+ }
+}
+
+int main(void)
+{
+ int flag = 0,target;
+ int num[10] = {1,2,3,4,5,6,7,8,10};
+ while(1)
+ {
+  printf("请输入您要查找的数字:\n");
+  scanf("%d",&target);
+  flag = Bin_Search(num,10,target);
+  if(flag) printf("已经找到该数字!!下标为%d\n",flag);
+  else printf("无该数字!!\n"); 
+ }
+ return 0;
+}
